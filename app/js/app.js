e2d3745fe75d912b8ce1d1d57e194041b4bd738d
ptiringo <ptiringo@gmail.com>
2015-09-03 22:47:46 +0900

    Try angular controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module("ngQQIQ", []);
+var app = angular.module("qqiq", []);
+
+var music = {
+  title: "People's Life",
+  releaseDate: "2013/04/28",
+  composer: "橋爪 裕",
+  songwriter: "橋爪 裕"
+};
+
+app.controller("MusicController", function() {
+  this.list = music;
+});
