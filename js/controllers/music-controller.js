9ab4180d021c13e6a59911a29c7e1fa5fbc44219
ptiringo <ptiringo@gmail.com>
2015-09-27 19:10:10 +0900

    Update 2015-09-27T10:09:53.957Z

---
+++ js/controllers/music-controller.js
@@ -0,0 +1,10 @@
+(function() {
+  angular.module("QQIQ")
+    .controller("MusicController", ['$http', function($http) {
+      var controller = this;
+      controller.musics = [];
+      $http.get('/musics.json').success(function(data) {
+        controller.musics = data;
+      });
+    }]);
+})();
---
+++ js/directives/navbar-directive.js
@@ -0,0 +1,15 @@
+(function() {
+  angular.module("QQIQ")
+    .directive("navbar", function() {
+      return {
+        restrict: "E",
+        templateUrl: "/templates/pages/navbar.html",
+        controller: ['$route', function($route) {
+          this.isSelected = function(page) {
+            return page == $route.current.activeTab;
+          };
+        }],
+        controllerAs: "navbar"
+      };
+    });
+})();
